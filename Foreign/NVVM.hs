f56914fae22225ee2136d1c1ee603b06e1594d33
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-06-01 17:41:46 +1000

    add top-level module

---
+++ Foreign/NVVM.hs
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- |
+-- Module    : Foreign.NVVM
+-- Copyright : [2016] Trevor L. McDonell
+-- License   : BSD
+--
+--------------------------------------------------------------------------------
+
+module Foreign.NVVM (
+
+  module Foreign.NVVM.Compile,
+  module Foreign.NVVM.Error,
+  module Foreign.NVVM.Info,
+
+) where
+
+import Foreign.NVVM.Compile
+import Foreign.NVVM.Error
+import Foreign.NVVM.Info
+
